aa2a4f21e9e52ae9785c55306b1dd1c16d8e4a6b
harleencore <harleen.k.sidhu@gmail.com>
2018-06-23 16:45:16 +0800

    adding simple line graph

--- app/src/main/java/harleen/tutorassist/StudentAnalyticsActivity.java
+++ app/src/main/java/harleen/tutorassist/StudentAnalyticsActivity.java
@@ -1,9 +1,16 @@
 package harleen.tutorassist;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class StudentAnalyticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +23,30 @@ public class StudentAnalyticsActivity extends AppCompatActivity {
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
+        textView.setText("Student " + message + "'s performance graph");
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+        // styling series
+        series.setTitle("Random Curve 1");
+        series.setColor(Color.GREEN);
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(10);
+        series.setThickness(8);
+
+// custom paint to make a dotted line
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(10);
+        paint.setPathEffect(new DashPathEffect(new float[]{8, 5}, 0));
+        series.setCustomPaint(paint);
     }
 
 }
